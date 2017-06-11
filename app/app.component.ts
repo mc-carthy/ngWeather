82bab5eed6fed0e0336da748ed345c831ee2349f
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-06-11 22:45:31 +0100

    Fix basic bugs to get app running

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    templateUrl: 'app.component.html'
+    template: '<h1>Skeleton App</h1>'
 })
 
 export class AppComponent  {
