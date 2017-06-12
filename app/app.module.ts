38ef2b293d0e3f003b633214d1a232e5b31466ab
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-06-12 23:56:46 +0100

    Create weather component

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './components/app/app.component';
+import { WeatherComponent } from './weather/components/weather.component';
 
 @NgModule({
     imports: [
         BrowserModule
     ],
     declarations: [
-        AppComponent
+        AppComponent,
+        WeatherComponent
     ],
     bootstrap: [
         AppComponent
---
+++ app/weather/components/weather.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'app-weather',
+    templateUrl: 'weather.component.html',
+    styleUrls: [ 'weather.component.css' ]
+})
+
+export class WeatherComponent {
+    
+    constructor() { }
+
+}
