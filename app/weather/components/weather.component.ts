0ca7905972e8b4d8970ebe09f869c198d136fb08
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-06-13 20:44:42 +0100

    Retrieve weather data from forecast.io

--- app/weather/components/weather.component.ts
+++ app/weather/components/weather.component.ts
@@ -6,14 +6,17 @@ import { WeatherService } from './../services/weather.service';
     moduleId: module.id,
     selector: 'app-weather',
     templateUrl: 'weather.component.html',
-    styleUrls: [ 'weather.component.css' ],
-    providers: [ WeatherService ]
+    styleUrls: ['weather.component.css'],
+    providers: [WeatherService]
 })
 
 export class WeatherComponent {
-    
+
     constructor(private service: WeatherService) {
         this.service.getCurrentLocation();
+        this.service.getCurrentWeather(51.5, 0)
+            .subscribe(weather => console.log(weather),
+            err => console.error(err));
     }
 
 }
--- app/weather/services/weather.service.ts
+++ app/weather/services/weather.service.ts
@@ -1,27 +1,40 @@
 import { Injectable } from '@angular/core';
+import { Jsonp } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
 
 import { FORECAST_KEY, FORECAST_ROOT } from './../constants/constants';
 
 @Injectable()
 export class WeatherService {
 
-    constructor() { }
+    constructor(private jsonp: Jsonp) { }
 
-    getCurrentLocation(): [number, number]
-    {
-        if (navigator.geolocation)
-        {
+    getCurrentLocation(): [number, number] {
+        if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition(pos => {
                 console.log("Position: ", pos.coords.latitude, ',', pos.coords.longitude); // TODO - Remove
                 return [pos.coords.latitude, pos.coords.longitude];
             },
-            err => console.error("Unable to get position - ", err));
+                err => console.error("Unable to get position - ", err));
         }
-        else
-        {
+        else {
             console.error("Geolocation service not available with this browser");
             return [51.5, 0];
         }
     }
-    
+
+    getCurrentWeather(lat: number, long: number): Observable<any> {
+        const url = FORECAST_ROOT + FORECAST_KEY + '/' + lat + ',' + long;
+        const queryParams = "?callback=JSONP_CALLBACK";
+
+        return this.jsonp.get(url + queryParams)
+            .map(res => res.json())
+            .catch(err => {
+                console.error("Unable to retrieve weather data - ", err);
+                return Observable.throw(err.json());
+            });
+    }
+
 }
