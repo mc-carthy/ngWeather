9cc4466c80452b2e2169aec6850a5c2174c52cea
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-06-13 08:05:36 +0100

    Injecting WeatherService

--- app/weather/components/weather.component.ts
+++ app/weather/components/weather.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 
+import { WeatherService } from './../services/weather.service';
+
 @Component({
     moduleId: module.id,
     selector: 'app-weather',
     templateUrl: 'weather.component.html',
-    styleUrls: [ 'weather.component.css' ]
+    styleUrls: [ 'weather.component.css' ],
+    providers: [ WeatherService ]
 })
 
 export class WeatherComponent {
     
-    constructor() { }
+    constructor(private service: WeatherService) { }
 
 }
