88bef6adc78b8a993825cb7cbdaf51c65d7d3cd7
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-06-17 15:32:09 +0100

    Fix speed unit pipe conversion

--- app/weather/pipes/speed-unit.pipe.ts
+++ app/weather/pipes/speed-unit.pipe.ts
@@ -9,7 +9,7 @@ export class SpeedUnitPipe implements PipeTransform {
         switch(unitType)
         {
             case "mph":
-                const miles = Number(speed * 1.61).toFixed(0);
+                const miles = Number(speed / 1.61).toFixed(0);
                 return miles + " mph";
             default:
                 return Number(speed).toFixed(0) + " kph";
